14467bf75ac919643f4a588d4b1e84a13b1784e4
Noëlie Ahuir <noelie.ahuir@edu.univ-fcomte.fr>
2024-11-27 11:48:25 +0100

    referme les détails suite à un clic sur un summary

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 // *** ACCORDEON ***
+const accordeon = document.querySelectorAll(".accordeon > details");
+
+accordeon.forEach((targetAccordeon) => {
+    targetAccordeon.addEventListener("click", () => {
+        accordeon.forEach((accordeon) => {
+            if (accordeon !== targetAccordeon) {
+                accordeon.removeAttribute("open");
+            }
+        });
+    });
+});
